13c4bcea454ed8643e77383dfa229480e9d30a92
lucarlig <luca.carlig@huawei.com>
2023-06-09 12:55:55 +0800

    add failure counter

--- benchmark2/benchmark.sh
+++ benchmark2/benchmark.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+count=0
+counter=0
+
 for d in */; do
   (cd "$d" && crusts && cargo +nightly check)
+  counter=$((counter + 1))
+  if [[ $? -eq 0 ]]; then
+    count=$((count + 1))
+  fi
+  echo "Total $counter times"
+  echo "Failed $count times" 
 done
+
+echo "Total $counter times"
+echo "Failed $count times"
