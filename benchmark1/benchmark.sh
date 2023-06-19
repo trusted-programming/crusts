1b32292cd69e287d873d3fd6060bdcd6ffb38837
lucarlig <luca.carlig@huawei.com>
2023-06-19 07:33:32 +0000

    fix bug with benchmark1

--- benchmark1/benchmark.sh
+++ benchmark1/benchmark.sh
@@ -5,7 +5,7 @@ failed_count=0
 total_count=0
 
 for d in */; do
-  (cd "$d" && crusts && cargo +nightly check)
+  (cd "${d}i" && crusts && cargo +nightly check)
   total_count=$((total_count + 1))
   if [[ $? -eq 0 ]]; then
     sucess_count=$((sucess_count + 1))
