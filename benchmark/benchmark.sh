893d31a4da7a5607ee2d09b0e053239aa0366fe1
lucarlig <luca.carlig@huawei.com>
2023-06-19 12:55:20 +0000

    add kloc metrics

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -24,6 +24,8 @@
 success_count=0
 failed_count=0
 total_count=0
+n_warnings=0
+code_count=0
 success_folders=""
 failed_folders=""
 
@@ -35,6 +37,11 @@ for d in */; do
   if [ $? -eq 0 ]; then
     success_count=$((success_count + 1))
     success_folders="${success_folders}${d%/}\n"
+    project_n_warnings=$(cd "${d}" && cargo clippy --message-format=json 2>&1 | grep -c 'warning:')
+    n_warnings=$(($n_warnings + $project_n_warnings - 1))
+    project_code_count=$(tokei -t=Rust --output=json ${d} | jq -r '.Rust.code')
+    code_count=$(($code_count + $project_code_count))
+
   else
     failed_count=$((failed_count + 1))
     failed_folders="${failed_folders}${d%/}\n"
@@ -45,6 +52,6 @@ for d in */; do
   echo "Total $total_count times"
   echo "Successful $success_count times"
   echo "Failed $failed_count times"
+  echo "Total Number of lines of Rust code: $code_count"
+  echo "Number of total warnings so far: $n_warnings"
 done
-
-
