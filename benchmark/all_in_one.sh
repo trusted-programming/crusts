b02a15d33ad0a603cf9b81463e829876dab6bc71
lucarlig <luca.carlig@huawei.com>
2023-07-05 06:51:05 +0000

    everything in one directly to MD file

--- benchmark/all_in_one.sh
+++ benchmark/all_in_one.sh
@@ -11,7 +11,7 @@ for d in */; do
     echo "Started directory: ${d}"
 
     # Run crusts
-    (cd "${d}" && crusts >/dev/null 2>&1)
+    (cd "${d}" && crusts) >>crusts.log 2>&1
 
     # REMOVE UNSAFE WARNINGS
     # Use find to find all .rs files recursively in the given folder
@@ -29,7 +29,7 @@ for d in */; do
     done
 
     # run clippy fix
-    (cd "${d}" && cargo +nightly-2023-06-02 clippy --fix --allow-dirty --allow-no-vcs >/dev/null 2>&1)
+    (cd "${d}" && cargo +nightly-2023-06-02 clippy --fix --allow-dirty --allow-no-vcs) >>clippy_fix.log 2>&1
 
     # filter out non compiling
     if [ $? -eq 0 ]; then
@@ -37,7 +37,7 @@ for d in */; do
         success_folders="${success_folders}${d%/}\n"
 
         # run clippy with 30 warninings denied
-        (cd "${d}" && cargo +nightly-2023-06-02 clippy -- -A clippy::all -A warnings -D non_snake_case -D non_upper_case_globals -D non_camel_case_types -D missing_debug_implementations -D clippy::wrong_self_convention -D clippy::missing_errors_doc -D clippy::missing_panics_doc -D clippy::undocumented_unsafe_blocks -D clippy::missing_safety_doc -D clippy::approx_constant -D clippy::borrow_interior_mutable_const -D clippy::declare_interior_mutable_const -D clippy::default_numeric_fallback -D clippy::bool_assert_comparison -D clippy::bool_comparison -D clippy::blocks_in_if_conditions -D clippy::arithmetic_side_effects -D clippy::needless_range_loop -D clippy::recursive_format_impl -D clippy::precedence -D clippy::bad_bit_mask -D clippy::match_overlapping_arm -D clippy::large_types_passed_by_value -D clippy::derived_hash_with_manual_eq -D clippy::derive_ord_xor_partial_ord -D clippy::from_over_into -D clippy::unwrap_used -D clippy::expect_used -D clippy::wildcard_imports -D clippy::mut_from_ref -D clippy::mutex_atomic -D clippy::mutex_integer -D clippy::redundant_allocation)
+        (cd "${d}" && cargo +nightly-2023-06-02 clippy -- -A clippy::all -A warnings -D non_snake_case -D non_upper_case_globals -D non_camel_case_types -D missing_debug_implementations -D clippy::wrong_self_convention -D clippy::missing_errors_doc -D clippy::missing_panics_doc -D clippy::undocumented_unsafe_blocks -D clippy::missing_safety_doc -D clippy::approx_constant -D clippy::borrow_interior_mutable_const -D clippy::declare_interior_mutable_const -D clippy::default_numeric_fallback -D clippy::bool_assert_comparison -D clippy::bool_comparison -D clippy::blocks_in_if_conditions -D clippy::arithmetic_side_effects -D clippy::needless_range_loop -D clippy::recursive_format_impl -D clippy::precedence -D clippy::bad_bit_mask -D clippy::match_overlapping_arm -D clippy::large_types_passed_by_value -D clippy::derived_hash_with_manual_eq -D clippy::derive_ord_xor_partial_ord -D clippy::from_over_into -D clippy::unwrap_used -D clippy::expect_used -D clippy::wildcard_imports -D clippy::mut_from_ref -D clippy::mutex_atomic -D clippy::mutex_integer -D clippy::redundant_allocation) >>clippy.log 2>&1
     else
         failed_count=$((failed_count + 1))
         failed_folders="${failed_folders}${d%/}\n"
@@ -52,11 +52,35 @@ else
     success_percentage=0.00
 fi
 
-echo -e "Success folders:\n$success_folders"
-echo -e "Failed folders:\n$failed_folders"
+warnings_string=$(grep -i 'error:' clippy.log | grep -iv 'could not compile')
 
-echo "Total runs: $total_runs"
-echo "Successful runs: $success_count"
-echo "Failed runs: $failed_count"
+warnings=$(echo "$warnings_string" | wc -l)
 
-echo "Successful runs percentage: $success_percentage%"
+echo "**Warnings**: $warnings" >>README.md
+
+# read line count from the directory
+lines=$(tokei -t=Rust --output=json ${d} | jq -r '.Rust.code')
+
+echo "**Lines**: $lines" >>README.md
+
+# getting number of lines in thousands
+lines_in_k=$(echo "scale=3; $lines / 1000" | bc -l)
+
+# calculate number of warnings per 1000 lines
+warnings_per_k=$(echo "scale=3; $warnings / $lines_in_k" | bc -l)
+
+echo "**KLOC**: $warnings_per_k" >>README.md
+
+warnings_types=$(echo "$warnings_string" | sort | uniq -c | sort -nr)
+
+echo "**Warning types**:" >>README.md
+echo "$warnings_types" >>README.md
+
+echo -e "**Success folders**:\n$success_folders" >>README.md
+echo -e "**Failed folders**:\n$failed_folders" >>README.md
+
+echo "**Total runs**: $total_runs" >>README.md
+echo "**Successful runs**: $success_count" >>README.md
+echo "**Failed runs**: $failed_count" >>README.md
+
+echo "**Successful runs percentage**: $success_percentage%" >>README.md
